83c3826acb134095e67b14723b26d55a1ec8ff19
Marc Norris <marcnorris007@icloud.com>
2023-03-15 20:33:21 +0000

    added about and contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Contact from './Components/Contact';
 import Card from './Components/Card';
+import About from './about';
 
 function App() {
   return (
@@ -14,3 +15,33 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="min-h-screen">
+//         <Header />
+//         {/* Wrap Route elements in a Routes component */}
+//         <Routes basename="/react-portfolio-new">
+//           {/* Define routes using the Route component to render different page components at different paths */}
+//           {/* Define a default route that will render the Home component */}
+//           <Route path="/react-portfolio-new/" element={<Home />} />
+//           <Route path="/react-portfolio-new/about" element={<About />} />
+//           <Route path="/react-portfolio-new/projects" element={<Projects />} />
+//           <Route path="/react-portfolio-new/gallery" element={<Gallery />} />
+//           {/* Define a route that will have descendant routes */}
+//           <Route path="/react-portfolio-new/contact/*" element={<Contact />} />
+//         </Routes>
+//       </div>
+//       <Footer></Footer>
+//     </Router>
+    
+//   );
+// }
+
+// export default App;

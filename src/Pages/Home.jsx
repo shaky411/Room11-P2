1a96660f0c3b70147920e6df5d6f3c4ee94d483b
Lee Vine <leedvine@gmail.com>
2023-03-16 19:25:51 +0000

    Created search, installed Axios and get request working to console

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchComponent from "../Components/SearchComponent";
 
 export default function Home() {
   return (
-    <div className="container flex flex-col justify-center items-center m-5 min-h-screen rounded-lg text-slate-200">
-      <h1 className="bg-indigo-400 p-6 rounded shadow" >Home Page</h1>
+    <div >
+      <SearchComponent></SearchComponent>
     </div>
   );
 }

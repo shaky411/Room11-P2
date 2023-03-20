435458a0fe3ca2d69d1e8c672d598bbc6ddc5497
Razzle Dazzle <razzledazzle@Razzles-MacBook-Pro.local>
2023-03-20 20:16:50 +0000

    carousel working - shit loads of code

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,58 +1,74 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Carousel from "./Carousel";
 
 export default function Hero() {
-  // state intitalisation for 'events' and 'heroImage' variable using 'useState'
   const [events, setEvents] = useState([]);
-  const [venues, setVenues] = useState([]);
-  // const [heroImage, setHeroImage] = useState([]);
-  //an asynch func to fethc the data
+  // const [imageUrl,setimageUrl]=useState('');
+  // const [eventName,seteventName]=useState('');
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        //storing and hiding api key in api variable
-        const api = process.env.REACT_APP_API_KEY;
-        const response = await axios.get(
-          `https://app.ticketmaster.com/discovery/v2/suggest?apikey=${api}&locale=*&size=5`
-        );
-        //setting the 'events' state variable tot he event data resposnse
-        setEvents(response.data._embedded.events);
-        console.log(response.data._embedded.events)
-        setVenues(response.data._embedded.venues);
-        //setting the 'heriImage' state variable to the event data response
-        // setHeroImage(response.data._embedded.events[0].images[0].url);
-      } catch (error) {
+    const api = process.env.REACT_APP_API_KEY;
+    fetch(
+      `https://app.ticketmaster.com/discovery/v2/suggest?apikey=${api}&locale=*&size=5`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setEvents(data._embedded.events);
+        console.log(data);
+      })
+      .catch((error) => {
         console.log(error);
-      }
-    };
-    fetchData();
+      });
   }, []);
 
+  const eventList = events.map((event) => {
+    const maxImage = event.images.reduce((max, img) =>
+      img.width > max.width ? img : max
+    );
+    return {
+      // create state for these and
+      name: event.name,
+      imageUrl: maxImage.url,
+    };
+  });
+  console.log(eventList);
+
+  //   const slides = eventList.map((event) => {
+  //     return {
+  //       name: event.name,
+  //       url: event.imageUrl
+  //     };
+  //   });
+  //   console.log(slides);
+
   return (
-    <div>
-      {/* <img src={heroImage} alt="" /> */}
-      {events.map((event) => (
-        <div key={event.id}>
-          <h2>{event.name}</h2>
-          {/* rending the largest image from the events->images array comparing the width of the current img ot the previous and return the ur of the largest one  */}
-          <img
-            src={
-              event.images.reduce((max, img) =>
-                img.width > max.width ? img : max
-              ).url
-            }
-            alt=""
-          />
-        </div>
-      ))}
-      <div>
-        {/* maping the venues by name we can brek it down to show other info also */}
-        {venues.map((venue) => (
-          <div key={venue.id}>
-            <h2>{venue.name}</h2>
-          </div>
-        ))}
-      </div>
-    </div>
+    <div><div>{eventList && <Carousel slides={eventList} />}</div></div>
+    // <div>
+    //   <div className="flex flex-col md:flex-row">
+    //     {/* <img src={heroImage} alt="" /> */}
+    //     {events.map((event) => (
+    //       <div key={event.id}>
+    //         <h2>{event.name}</h2>
+    //         {/* rending the largest image from the events->images array comparing the width of the current img ot the previous and return the ur of the largest one  */}
+    //         <img
+    //           className="hover:scale-125 transition-scale duration-700 shadow-lg"
+    //           src={
+    //             event.images.reduce((max, img) =>
+    //               img.width > max.width ? img : max
+    //             ).url
+    //           }
+    //           alt=""
+    //         />
+    //       </div>
+    //     ))}
+    //   </div>
+      
+    // </div>
   );
 }

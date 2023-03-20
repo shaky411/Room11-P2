4db8f769ec21485e6a3e7f6d0cca69b6d4969c78
Razzle Dazzle <razzledazzle@Razzles-MacBook-Pro.local>
2023-03-20 21:55:18 +0000

    issues resolved with api and carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -24,8 +24,9 @@ function Carousel({ slides }) {
 
   return (
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+        <h2 className=" bottom-0 left-0 right-0  py-2 px-4"> {slides[currentIndex].name} </h2>
         <img className='w-full h-full rounded-2xl bg-center bg-cover duration-500' src= {slides[currentIndex].imageUrl} alt="..." />
-        <h2 className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-0 text-white py-2 px-4"> {slides[currentIndex].name} </h2>
+        
 
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
--- src/Components/GitWidget3.jsx
+++ src/Components/GitWidget3.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+
 export default function GitWidget3() {
   const [githubData, setGithubData] = useState([]);
   const [githubData2, setGithubData2] = useState([]);

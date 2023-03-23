2b5b229a0ca369ea56aba4a7874911099c6b7061
Marc Norris <85554579+gekko1@users.noreply.github.com>
2023-03-23 06:15:38 +0000

    updated about page margins and content

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,12 @@ import GitWidget3 from "../Components/GitWidget3";
 export default function About() {
   return (
     
+    <div>
       <GitWidget3></GitWidget3>
+      <div className="flex justify-center items-center text-center w-1/2">
+      
+      </div>
+      </div>
     
   );
 }

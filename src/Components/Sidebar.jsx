2e6043d6fa13161f7e14b20c819d404bdf0a3ade
Marc Norris <shaky411@googlemail.com>
2023-03-24 11:43:38 +0000

    added react-burger-menu

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { bubble as Menu } from 'react-burger-menu';
+import '../../src/sidebar.css'
+
+export default props => {
+  return (
+    <Menu width={300}>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+      <a className="menu-item" href="/about">
+        About
+      </a>
+      <a className="menu-item" href="/contact">
+        Contact
+      </a>
+    </Menu>
+  );
+};
